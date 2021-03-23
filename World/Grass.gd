b960a1afeb216c650556af5291f97b57e7bc355a
Uzair Ashraf <uzinatorcl@gmail.com>
2021-03-22 23:56:47 -0700

    Setup collisions and attack animations

--- World/Grass.gd
+++ World/Grass.gd
@@ -3,9 +3,11 @@ extends Node2D
 onready var GrassEffect = load("res://Effects/GrassEffect.tscn")
 onready var main = get_tree().current_scene
 
-func _process(delta):
-		if Input.is_action_pressed("ui_select"):
-			var grassEffect = GrassEffect.instance()
-			main.add_child(grassEffect)
-			grassEffect.global_position = self.global_position
-			queue_free()
+func _on_HurtBox_area_entered(area):
+		triggerGrassEffect()
+		queue_free()
+
+func triggerGrassEffect():
+	var grassEffect = GrassEffect.instance()
+	main.add_child(grassEffect)
+	grassEffect.global_position = self.global_position
